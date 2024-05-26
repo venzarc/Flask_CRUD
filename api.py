1e16480bffaf892b2317ab00047b1ae039909d85
venzarc <202040224@psu.palawan.edu.ph>
2024-05-26 13:00:20 +0800

    Added api.py file

---
+++ api.py
@@ -0,0 +1,18 @@
+from flask import Flask, make_response, jsonify, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config["MYSQL_HOST"] = "127.0.0.1"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "1234"
+app.config["MYSQL_DB"] = "mydb"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"  
+
+mysql = MySQL(app)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
