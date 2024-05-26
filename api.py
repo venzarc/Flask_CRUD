585e40ee83add378b5e303e13382398ffd573086
venzarc <202040224@psu.palawan.edu.ph>
2024-05-26 13:22:40 +0800

    Added functions to select account details and customer tables

--- api.py
+++ api.py
@@ -30,6 +30,20 @@ def get_net():
     data = data_fetch(query)
     return make_response(jsonify(data), 200)
 
+#select function for customers
+@app.route("/customers", methods=["GET"])
+def get_customer():
+    query = "SELECT * FROM customer"
+    data = data_fetch(query)
+    return make_response(jsonify(data), 200)
+
+#select function for account details
+@app.route("/account_details", methods=["GET"])
+def get_account():
+    query = "SELECT * FROM account_details"
+    data = data_fetch(query)
+    return make_response(jsonify(data), 200)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
